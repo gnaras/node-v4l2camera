00373eab98ed717c362f441694a0d338724c4dd2
sarang <sarang@gokhale.be>
2018-03-17 19:41:04 -0700

    removed png conversion from image-stream server example, added option to not have stop callback

--- examples/image-stream-server.js
+++ examples/image-stream-server.js
@@ -1,5 +1,4 @@
 var http = require("http");
-var pngjs = require("pngjs");
 var v4l2camera = require("../");
 
 var server = http.createServer(function (req, res) {
@@ -13,18 +12,18 @@ var server = http.createServer(function (req, res) {
             "<html><head><meta charset='utf-8'/>",
             "<script>(", script.toString(), ")()</script>",
             "</head><body>",
-            "<img id='cam' width='352' height='288' />",
+            "<img id='cam' width='640' height='480' />",
             "</body></html>",
         ].join(""));
         return;
     }
-    if (req.url.match(/^\/.+\.png$/)) {
+    if (req.url.match(/^\/.+\.jpg$/)) {
         res.writeHead(200, {
-            "content-type": "image/png",
+            "content-type": "image/jpg",
             "cache-control": "no-cache",
         });
-        var png = toPng();
-        return png.pack().pipe(res);
+        var jpg = cam.frameRaw();
+        return res.end(Buffer(jpg));
     }
 });
 server.listen(3000);
@@ -40,33 +39,17 @@ var script = function () {
                 cam = img;
                 load();
             }, false);
-            img.src = "/" + Date.now() + ".png";
+            img.src = "/" + Date.now() + ".jpg";
         })();
     }, false);
 };
 
-var toPng = function () {
-    var rgb = cam.toRGB();
-    var png = new pngjs.PNG({
-        width: cam.width, height: cam.height,
-        deflateLevel: 1, deflateStrategy: 1,
-    });
-    var size = cam.width * cam.height;
-    for (var i = 0; i < size; i++) {
-        png.data[i * 4 + 0] = rgb[i * 3 + 0];
-        png.data[i * 4 + 1] = rgb[i * 3 + 1];
-        png.data[i * 4 + 2] = rgb[i * 3 + 2];
-        png.data[i * 4 + 3] = 255;
-    }
-    return png;
-};
-
 var cam = new v4l2camera.Camera("/dev/video0")
-if (cam.configGet().formatName !== "YUYV") {
-    console.log("YUYV camera required");
-    process.exit(1);
+for (var i = 0; i < cam.formats.length; i++) {
+    console.log(cam.formats[i].formatName + " " + cam.formats[i].width + "x" + cam.formats[i].height +
+                " " + cam.formats[i].interval.numerator + "/" + cam.formats[i].interval.denominator);
 }
-cam.configSet({width: 352, height: 288});
+cam.configSet(cam.formats[3]);
 cam.start();
 cam.capture(function loop() {
     cam.capture(loop);
