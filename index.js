e5ded5192c4fce320ce8502a511d23ad3b1c351e
sarang <sarang@gokhale.be>
2019-04-28 02:53:24 -0700

    small fix to debug run

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const DEBUG = process.env.DEBUG === 'true' ? true : false;
 var path = require('path');
-var raw = null
 if(DEBUG) {
-  raw = require(path.resolve( __dirname,"./build/Debug/v4l2camera"));
+  var raw = require(path.resolve( __dirname,"./build/Debug/v4l2camera"));
 } else {
-  raw = require(path.resolve( __dirname,"./build/Release/v4l2camera"));
+  var raw = require(path.resolve( __dirname,"./build/Release/v4l2camera"));
 }
 
 exports.Camera = raw.Camera;
