e8e531383613246a9334e5bfcd767f4fa2734544
sarang <sarang@gokhale.be>
2019-04-27 23:40:11 -0700

    use debug binary when in debug mode

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var raw = require("./build/Release/v4l2camera");
+const DEBUG = process.env.DEBUG === 'true' ? true : false;
+var path = require('path');
+var raw = null
+if(DEBUG) {
+  raw = require(path.resolve( __dirname,"./build/Debug/v4l2camera"));
+} else {
+  raw = require(path.resolve( __dirname,"./build/Release/v4l2camera"));
+}
 
 exports.Camera = raw.Camera;
